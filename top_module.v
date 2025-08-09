88d72475588034ab750113dd6e2e7c0bbc469327
naveenbitsathy-ece <naveenrajs1003@gmail.com>
2025-08-09 15:36:22 +0530

    top_module.v

---
+++ top_module.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+`include "clk.v"
+`include "res.v"
+module top_module(
+            input clk_in,  
+            input reset,
+            input data_in,     
+            input valid,       
+            input mode,   
+            output parity_ok,
+            output [3:0] counter
+    );
+    
+    wire clk;
+    clockdivider clk_div(.clk(clk),.clk_in(clk_in),.reset(reset));
+    parity parok(.clk(clk),.reset(reset),.valid(valid),.mode(mode),.data_in(data_in),.parity_ok(parity_ok),.counter(counter));
+    
+endmodule
